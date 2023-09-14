e82178fa1462fa31215d3d9a6bc9d6246c246f17
0715yk <yongkiniiii@gmail.com>
2023-09-14 17:09:15 +0900

    fix getDrawCursor

--- libs.ts
+++ libs.ts
@@ -4,29 +4,26 @@ export function getDrawCursor(
   strokeColor?: string
 ) {
   const circle = `
-      <svg
-        height="${strokeWidth}"
-        fill="${brushColor}"
-        viewBox="0 0 ${strokeWidth * 2} ${strokeWidth * 2}"
-        width="${strokeWidth}"
-        xmlns="http://www.w3.org/2000/svg"
-        stroke="${strokeColor ? strokeColor : "black"}"
-      >
-        <circle
-          cx="50%"
-          cy="50%"
-          r="${strokeWidth}"    
-          fill="${brushColor}"
-          stroke="${strokeColor ? strokeColor : "black"}"
-        />
-      </svg>
+  <svg
+  height="${strokeWidth}"
+  fill="${brushColor}"
+  viewBox="0 0 ${strokeWidth * 2} ${strokeWidth * 2}"
+  width="${strokeWidth}"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <circle
+    cx="50%"
+    cy="50%"
+    r="${strokeWidth}" 
+    stroke="${strokeColor ? strokeColor : "black"}"
+  />
+</svg>
     `;
 
   return `url(data:image/svg+xml;base64,${window.btoa(circle)}) ${Math.ceil(
     strokeWidth / 2
   )} ${Math.ceil(strokeWidth / 2)}, pointer`;
 }
-
 export function dataURItoBlob(dataURI: string) {
   const byteString = window.atob(dataURI.split(",")[1]);
   const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
